bf65bbf9a0bd2892001f767727d1389c9f694b2f
Sanika Sathe <sanikasathe32@gmail.com>
2026-02-24 22:45:28 +0530

    exp1,2,3 added

---
+++ lab_work/solid_principles/interface segregation principle/interface segregation principle/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Separate small interfaces
+interface IWork
+{
+    void Work();
+}
+
+interface IEat
+{
+    void Eat();
+}
+
+// Human can work and eat
+class Human : IWork, IEat
+{
+    public void Work()
+    {
+        Console.WriteLine("Human working");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Human eating");
+    }
+}
+
+// Robot only works
+class Robot : IWork
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot working");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IWork human = new Human();
+        human.Work();
+
+        IWork robot = new Robot();
+        robot.Work();
+    }
+}
---
+++ lab_work/solid_principles/liskov_substitution_principle/liskov_substitution_principle/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+//base class
+class Bird
+{
+    public virtual void Move()
+    {
+        Console.WriteLine("Bird moves");
+    }
+}
+//derived class
+class Sparrow : Bird
+{
+    public override void Move()
+    {
+        Console.WriteLine("Sparrow flies");
+    }
+}
+
+class Ostrich : Bird
+{
+    public override void Move()
+    {
+        Console.WriteLine("Ostrich walks");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {//apply lsp
+        Bird b1 = new Sparrow();
+        Bird b2 = new Ostrich();
+
+        b1.Move();
+        b2.Move();
+    }
+}
