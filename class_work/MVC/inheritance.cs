b86e0259f107ec1194a475a5b13c904b42a2b482
sanikasathe <sanikasathe32@gmail.com>
2026-04-28 18:53:53 +0530

    Create inheritance.cs

---
+++ class_work/MVC/inheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNET_Practice
+{
+    internal class Inheritance
+    {
+            private string brand = "Ford";
+
+            public string Brand
+            {
+                get { return brand;}
+            }
+
+            public void horn()
+            {
+                Console.WriteLine("Tuut, Tuut!!");
+            }
+        
+    }
+    class Vehicle: Inheritance
+    {
+        public string modelName = "Mustang";
+
+        public string ModelName
+        {
+            get { return modelName; }
+        }
+    }
+}
