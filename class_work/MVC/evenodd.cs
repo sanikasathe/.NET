434ae5d4a766a12f7636a7732dd79c6fd7bfd3f7
sanikasathe <sanikasathe32@gmail.com>
2026-04-28 18:49:27 +0530

    Add EvenOdd class to determine even or odd numbers

---
+++ class_work/MVC/evenodd.cs
@@ -0,0 +1,11 @@
+class EvenOdd{
+    public static void Main(){
+        int num = 10;
+
+        if(num % 2 == 0){
+            System.Console.WriteLine("The number " + num + " is Even.");
+        }else{
+            System.Console.WriteLine("The number " + num + " is Odd.");
+        }
+    }
+}
